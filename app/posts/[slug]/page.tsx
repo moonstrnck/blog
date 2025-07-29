d3c66657f928d1c0ea3a1cd976625bd18c21c568
moonstrunck <mshwi1211@gmail.com>
2025-07-30 00:12:03 +0900

    fix layout

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -25,7 +25,7 @@ export default async function BlogPost({ params }: Props) {
 
   return (
     <article className="container py-12">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-[240px_1fr_240px] md:gap-8">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-[1fr_240px] md:gap-8">
         <section>
           <PostHeader post={post} markdown={markdown} />
           <Separator className="mt-6 mb-6 md:mb-12" />
